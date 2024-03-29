61a8d2645a4e05aa6f500c135c81ff6c1bc08b08
Александр Донцов <doncov@scanex.ru>
2024-03-29 17:19:18 +0700

    праввка вёрстки

--- resources/views/components/main-slider.blade.php
+++ resources/views/components/main-slider.blade.php
@@ -2,6 +2,19 @@
 <div id="slides-shop" class="cover-slides">
     <ul class="slides-container">
         <li class="text-center">
+            <img src="images/banner/photobank.jpg" alt="">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="m-b-20"><strong>ОБОРУДОВАНИЕ ДЛЯ ПРОИЗВОДСТВА МАСЛА</strong></h1>
+                        <p class="m-b-40">Поставка, запчасти и сервисное обслуживание маслобойного оборудования<br>
+                            Проверенное оборудование от надёжных поставщиков</p>
+                        <p><a class="btn hvr-hover" href="#">Продукция</a></p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="text-center">
             <img src="images/banner/banner_01.jpg" alt="">
             <div class="container">
                 <div class="row">
@@ -16,19 +29,6 @@
         </li>
         <li class="text-center">
             <img src="images/banner/banner_02.jpg" alt="">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="m-b-20"><strong>ОБОРУДОВАНИЕ ДЛЯ ПРОИЗВОДСТВА МАСЛА</strong></h1>
-                        <p class="m-b-40">Поставка, запчасти и сервисное обслуживание маслобойного оборудования<br>
-                            Проверенное оборудование от надёжных поставщиков</p>
-                        <p><a class="btn hvr-hover" href="#">Продукция</a></p>
-                    </div>
-                </div>
-            </div>
-        </li>
-        <li class="text-center">
-            <img src="images/banner/banner_03.jpg" alt="">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
