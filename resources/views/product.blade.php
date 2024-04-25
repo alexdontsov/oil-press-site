ee3d835ed4bf0526c3abcc0bfa2d460aed71a03e
Александр Донцов <doncov@scanex.ru>
2024-04-25 16:47:50 +0700

    as

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -21,7 +21,7 @@
     <div class="shop-detail-box-main">
         <div class="container">
             <div class="row">
-                <div class="col-xl-7 col-lg-7 col-md-8">
+                <div class="col-xl-6 col-lg-6 col-md-7">
                     <div id="carousel-example-1" class="single-product-slider carousel slide" data-ride="carousel">
                         <div class="carousel-inner" role="listbox">
                             @foreach ($product->images as $image)
@@ -46,7 +46,7 @@
                         </ol>
                     </div>
                 </div>
-                <div class="col-xl-5 col-lg-5 col-md-4">
+                <div class="col-xl-6 col-lg-6 col-md-5">
                     <div class="single-product-details">
                         <h1>{{$product->title}}</h1>
                         Маслопресс предварительного отжима с возможностью регулировки толщины жмыха без остановки
