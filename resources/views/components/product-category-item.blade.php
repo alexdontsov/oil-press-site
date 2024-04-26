c0ebcd60d683397c79463501e8c34216219d5606
Александр Донцов <doncov@scanex.ru>
2024-04-26 15:53:29 +0700

    as

--- resources/views/components/product-category-item.blade.php
+++ resources/views/components/product-category-item.blade.php
@@ -1,6 +1,8 @@
 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
     <div class="shop-cat-box">
-        <img class="img-fluid" src="{{URL::asset('/app/' . $category->image)}}" alt="{{$category->description}}" />
+        <a style="position: relative" href="/catalog/{{$category->slug}}">
+            <img class="img-fluid" src="{{URL::asset('/app/' . $category->image)}}" alt="{{$category->description}}" />
+        </a>
         <a class="btn hvr-hover" href="/catalog/{{$category->slug}}">{{$category->title}}</a>
     </div>
 </div>
