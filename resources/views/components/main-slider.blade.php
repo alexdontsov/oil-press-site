09a487b31476e1a579225fe3f943fe65b0eb5a83
Александр Донцов <doncov@scanex.ru>
2024-06-17 17:32:13 +0700

    slider

--- resources/views/components/main-slider.blade.php
+++ resources/views/components/main-slider.blade.php
@@ -4,49 +4,10 @@
         @foreach($sliders as $slider)
             <x-main-slider-item :slider="$slider"></x-main-slider-item>
         @endforeach
-{{--        <li class="text-center">--}}
-{{--            <img src="images/banner/photobank.jpg" alt="">--}}
-{{--            <div class="container">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-md-12">--}}
-{{--                        <h1 class="m-b-20"><strong>ОБОРУДОВАНИЕ ДЛЯ ПРОИЗВОДСТВА МАСЛА</strong></h1>--}}
-{{--                        <p class="m-b-40">Поставка, запчасти и сервисное обслуживание маслобойного оборудования<br>--}}
-{{--                            Проверенное оборудование от надёжных поставщиков</p>--}}
-{{--                        <p><a class="btn hvr-hover" href="#">Продукция</a></p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </li>--}}
-{{--        <li class="text-center">--}}
-{{--            <img src="images/banner/banner_01.jpg" alt="">--}}
-{{--            <div class="container">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-md-12">--}}
-{{--                        <h1 class="m-b-20"><strong>ОБОРУДОВАНИЕ ДЛЯ ПРОИЗВОДСТВА МАСЛА</strong></h1>--}}
-{{--                        <p class="m-b-40">Поставка, запчасти и сервисное обслуживание маслобойного оборудования<br>--}}
-{{--                            Проверенное оборудование от надёжных поставщиков</p>--}}
-{{--                        <p><a class="btn hvr-hover" href="#">Продукция</a></p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </li>--}}
-{{--        <li class="text-center">--}}
-{{--            <img src="images/banner/banner_02.jpg" alt="">--}}
-{{--            <div class="container">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-md-12">--}}
-{{--                        <h1 class="m-b-20"><strong>ОБОРУДОВАНИЕ ДЛЯ ПРОИЗВОДСТВА МАСЛА</strong></h1>--}}
-{{--                        <p class="m-b-40">Поставка, запчасти и сервисное обслуживание маслобойного оборудования<br>--}}
-{{--                            Проверенное оборудование от надёжных поставщиков</p>--}}
-{{--                        <p><a class="btn hvr-hover" href="#">Продукция</a></p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </li>--}}
     </ul>
     <div class="slides-navigation">
-        <a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
-        <a href="#" class="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+        <a href="#" class="next d-none d-md-block"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+        <a href="#" class="prev d-none d-md-block""><i class="fa fa-angle-left" aria-hidden="true"></i></a>
     </div>
 </div>
 <!-- End Slider -->
